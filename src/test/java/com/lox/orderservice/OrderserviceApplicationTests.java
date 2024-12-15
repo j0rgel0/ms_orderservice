124b9d9bc96d41ab898020bd1286b82b0b6598ae
Jorge Lopez Valdez <jorge.lopezvz@udlap.mx>
2024-12-14 22:27:01 -0600

    Upload project

---
+++ src/test/java/com/lox/orderservice/OrderserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lox.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
